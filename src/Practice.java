c866f00e995ca794b9fa17dba1fb494725bcfd39
Lalit Siraswa <lalitsiraswa@Lalits-MacBook-Pro.local>
2024-12-07 13:32:46 +0530

    Initialization Block

--- src/Practice.java
+++ src/Practice.java
@@ -1,5 +1,23 @@
+abstract class Parent {
+    public String name;
+}
+
+class Child extends Parent {
+    // Initialization Block
+    {
+        name = "Lalit Siraswa";
+    }
+
+    void fun() {
+        System.out.println(name);
+    }
+}
+
 public class Practice {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        Child obj = new Child();
+        System.out.println(obj.name);
+        obj.fun();
     }
 }
