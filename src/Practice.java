8e7866838ad38c70c50c780a85cd26214fafc3c4
Lalit Siraswa <lalitsiraswa@Lalits-MacBook-Pro.local>
2024-12-08 19:06:58 +0530

    Read Input From Console

--- src/Practice.java
+++ src/Practice.java
@@ -1,23 +1,16 @@
-abstract class Parent {
-    public String name;
-}
-
-class Child extends Parent {
-    // Initialization Block
-    {
-        name = "Lalit Siraswa";
-    }
-
-    void fun() {
-        System.out.println(name);
-    }
-}
+import java.util.Scanner;
 
 public class Practice {
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        Child obj = new Child();
-        System.out.println(obj.name);
-        obj.fun();
+        // Using Scanner for Getting Input from User
+        Scanner inputScanner = new Scanner(System.in);
+        String str = inputScanner.nextLine();
+        System.out.println("You Entered String : " + str);
+
+        int x = inputScanner.nextInt();
+        System.out.println("You Entered Integer " + x);
+
+        float y = inputScanner.nextFloat();
+        System.out.println("You Entered Float : " + y);
     }
 }
