1b03d3ae3bd5ee9f19c6e2f547ec820cd6330bd0
Lalit Siraswa <lalitsiraswa@Lalits-MacBook-Pro-2.local>
2024-12-30 15:31:38 +0530

    LiskovSubstitutionPrincipleProblem

---
+++ src/solid_principles/liskov_substitution_principle_by_shrayanch/problem/LiskovSubstitutionPrincipleProblem.java
@@ -0,0 +1,40 @@
+package solid_principles.liskov_substitution_principle_by_shrayanch.problem;
+
+interface Bike {
+    void turnOnEngine();
+
+    void accelerate();
+}
+
+class MotorCycle implements Bike {
+    boolean isEngineOn;
+    int speed;
+
+    @Override
+    public void turnOnEngine() {
+//        Turn on the engine:
+        isEngineOn = true;
+    }
+
+    @Override
+    public void accelerate() {
+//        Increase the speed
+        speed += 10;
+    }
+}
+
+class Bicycle implements Bike {
+
+    @Override
+    public void turnOnEngine() {
+        throw new AssertionError("there is no engine!");
+    }
+
+    @Override
+    public void accelerate() {
+//        Do something
+    }
+}
+
+public class LiskovSubstitutionPrincipleProblem {
+}
